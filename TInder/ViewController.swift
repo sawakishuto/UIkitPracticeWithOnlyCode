40a58b3fe3d8fea229ce56b0d8fe61ca33b2bbf8
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-10 17:45:32 +0900

    カードの傾き追加

--- TInder/ViewController.swift
+++ TInder/ViewController.swift
@@ -27,7 +27,23 @@ class ViewController: UIViewController {
         let point = sender.translation(in: view)
 
         card.center = CGPoint(x: card.center.x + point.x, y: card.center.y + point.y)
+
+        let xFromCenter = card.center.x - view.center.x
+        card.transform = CGAffineTransform(rotationAngle: xFromCenter / (view.frame.width / 2) * -0.785)
         if sender.state == UIGestureRecognizer.State.ended {
+            if card.center.x < 75 {
+                UIView.animate(withDuration: 0.2) {
+                    card.center = CGPoint(x: card.center.x - 250, y: card.center.y)
+                    return
+                }
+            } else if card.center.x > self.view.frame.width - 75 {
+                UIView.animate(withDuration: 0.2) {
+                    card.center = CGPoint(x: card.center.x + 250, y: card.center.y)
+
+                }
+                return
+            }
+
             UIView.animate(withDuration: 0.2) {
                 card.center = self.cardOfCenter
             }
