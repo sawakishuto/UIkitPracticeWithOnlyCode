6e7bbacde7bb7db76cd9fc80d9ff69789f315575
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-10 17:25:04 +0900

    カードを動かす

--- TInder/ViewController.swift
+++ TInder/ViewController.swift
@@ -24,7 +24,9 @@ class ViewController: UIViewController {
 
     @IBAction func swipeCard(_ sender: UIPanGestureRecognizer) {
         let card = sender.view!
+        let point = sender.translation(in: view)
 
+        card.center = CGPoint(x: card.center.x + point.x, y: card.center.y + point.y)
     }
     
 }
